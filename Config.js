54c4f03f3e2b0460d78a6ee0f2fc406449d5d589
NkMasy <kreizosorhey@gmail.com>
2024-05-15 20:03:32 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,7 +3,7 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
+global.ownernumber = [ process.env.OWNER_NUMBER || "7638942753"],
 global.ownername = process.env.OWNER_NAME || "Neipekreizo",
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
